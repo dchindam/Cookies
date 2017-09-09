7ad9dbbcf3f109c8f66829a15b3453e49169a964
DELL <DELL@192.168.0.8>
2017-09-09 15:24:00 -0400

    Changed to D

--- src/main/java/myfirstmaven/HelloMaven/App.java
+++ src/main/java/myfirstmaven/HelloMaven/App.java
@@ -4,7 +4,7 @@ public class App {
 	public static void main(String[] args) {
 
 		System.out.println(getHelloDivya());
-		//System.out.println(getHelloDSC());
+		System.out.println(getHelloName("D"));
 
 	}
 
